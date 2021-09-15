8e8eb62d2f7857ebd3aaee8cef760767daf86a39
Gao Sun <gao@silverhand.io>
2021-09-16 01:02:52 +0800

    refactor: use pnpm and upgrade packages

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { MemoryRouter } from 'react-router-dom';
+
 import App from './App';
 
 describe('<App />', () => {
--- src/client.tsx
+++ src/client.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable unicorn/prefer-module */
-import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
 import App from './App';
 
 const render = module.hot ? ReactDOM.render : ReactDOM.hydrate;
